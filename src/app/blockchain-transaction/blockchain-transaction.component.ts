fdcbe2ed5fcdd1fd0601592ac791e3dc9447329c
E.Iliadis <vagelisiliadis93@gmail.com>
2023-04-04 21:39:12 +0300

    Transcaction implementations

--- src/app/blockchain-transaction/blockchain-transaction.component.ts
+++ src/app/blockchain-transaction/blockchain-transaction.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import { MetamaskService } from '../service/metamask.service';
+
+interface CoinVariant {
+  name: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-blockchain-transaction',
   templateUrl: './blockchain-transaction.component.html',
-  styleUrls: ['./blockchain-transaction.component.scss']
+  styleUrls: ['./blockchain-transaction.component.scss'],
 })
 export class BlockchainTransactionComponent implements OnInit {
+  public fromAddress!: string;
+  public toAddress!: string;
+  public amount!: number;
+  public coinVariants: CoinVariant[] = [
+    { name: 'Ethereum', value: 'ETH' },
+    { name: 'Bitcoin', value: 'BTC' },
+    { name: 'Binance Coin', value: 'BNB' },
+    { name: 'Tether', value: 'USDT' },
+  ];
+  public selectedCoin: string = 'ETH';
+  address!: string;
+  transactions: any[] = [];
 
-  constructor() { }
+  constructor(private metamaskService: MetamaskService) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.address = await this.metamaskService.getAddress();
+    await this.loadTransactions();
   }
 
+  async onSubmit() {
+    try {
+      await this.metamaskService.connect();
+
+      this.fromAddress = await this.metamaskService.getAddress();
+      const toAddress = this.toAddress;
+      const amount = Number(this.amount);
+      const nonce = await this.metamaskService.getNonce(this.fromAddress);
+
+      const newTransaction = {
+        to: toAddress,
+        value: amount,
+        nonce: Number(nonce),
+        status: 'pending',
+        errorMessage: '',
+      };
+
+      this.transactions.unshift(newTransaction); // add new transaction to beginning of array
+
+      await this.metamaskService.sendTransaction(newTransaction); // send transaction
+
+      newTransaction.status = 'success';
+
+      this.toAddress = '';
+      this.amount = 0;
+    } catch (error: any) {
+      console.error(error);
+
+      // find most recent transaction in array and add error message
+      const mostRecentTransaction = this.transactions[0];
+      if (mostRecentTransaction) {
+        mostRecentTransaction.status = 'failed';
+        mostRecentTransaction.errorMessage = error.message;
+      }
+    } finally {
+      await this.metamaskService.disconnect();
+    }
+  }
+
+  async loadTransactions(): Promise<void> {
+    try {
+      const txs = await this.metamaskService.getTransactions();
+      this.transactions = txs.map((tx: any) => {
+        const date = new Date(parseInt(tx.timeStamp) * 1000);
+        const coinType = this.selectedCoin;
+        return {
+          hash: tx.hash,
+          from: tx.from,
+          to: tx.to,
+          value: tx.value,
+          gasPrice: tx.gasPrice,
+          gasUsed: tx.gasUsed,
+          blockNumber: tx.blockNumber,
+          date,
+          coinType,
+        };
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }  
 }
--- src/app/service/metamask.service.ts
+++ src/app/service/metamask.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
 import Web3 from 'web3';
 
 interface CustomWindow extends Window {
@@ -12,7 +13,6 @@ export class MetamaskService {
   private web3?: Web3;
   private readonly customWindow: CustomWindow;
 
-
   constructor() {
     this.customWindow = window as CustomWindow;
     if (this.customWindow.ethereum) {
@@ -23,13 +23,15 @@ export class MetamaskService {
   }
 
   async connect() {
-    try{
-      await this.customWindow.ethereum.request({ method: 'eth_requestAccounts' });
+    try {
+      await this.customWindow.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+    } catch {
+      throw new Error(
+        'Please install metamask browser addon in order to continue!'
+      );
     }
-    catch{
-      throw new Error('Please install metamask browser addon in order to continue!');
-    }
-
   }
 
   async isConnected(): Promise<boolean> {
@@ -61,4 +63,52 @@ export class MetamaskService {
   isMetamaskInstalled(): boolean {
     return Boolean(this.customWindow.ethereum);
   }
+
+  async getAccount(): Promise<string> {
+    const account = await this.getAddress();
+    return account;
+  }
+
+  async sendTransaction(transaction: { to: string, value: number, nonce: number }) {
+    const fromAddress = await this.getAccount();
+    const gasPrice = await this.web3?.eth.getGasPrice();
+    const gasLimit = 21000;
+    const data = '';
+    const nonce = transaction.nonce;
+  
+    const tx = {
+      from: fromAddress,
+      to: transaction.to,
+      value: transaction.value,
+      gasPrice,
+      gasLimit,
+      data,
+      nonce,
+    };
+  
+    const signedTx = await this.customWindow.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [tx],
+    });
+  
+    return signedTx;
+  }
+
+  async getTransactions(): Promise<any[]> {
+    const address = await this.getAddress();
+    const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=desc&apikey=<YOUR_API_KEY>`;
+    const response = await fetch(url);
+    const json = await response.json();
+    if (json.status === "1") {
+      return json.result;
+    } else {
+      throw new Error("Error fetching transactions");
+    }
+  }
+  
+  async getNonce(fromAddress: string): Promise<number> {
+    const nonce : any = await this.web3?.eth.getTransactionCount(fromAddress, 'pending');
+    return nonce;
+  }
 }
+
